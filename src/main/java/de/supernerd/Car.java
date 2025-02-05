6f28c38f27f066c3053765314bd4576a8e65370a
Manuel Simon <info@der-supernerd.de>
2025-02-05 15:38:00 +0100

    create bonus task

--- src/main/java/de/supernerd/Car.java
+++ src/main/java/de/supernerd/Car.java
@@ -1,4 +1,59 @@
 package de.supernerd;
 
+import java.util.Objects;
+
 public class Car {
+    private static int totalCount = 0;
+    private int instanceCount = 0;
+
+    public Car() {
+    }
+
+    public Car(int instanceCount) {
+        this.instanceCount = instanceCount;
+    }
+
+    public static void incrementTotalCount() {
+        totalCount++;
+    }
+
+    public void incrementInstanceCount() {
+        instanceCount++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return instanceCount == car.instanceCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(instanceCount);
+    }
+
+    public static int getTotalCount() {
+        return totalCount;
+    }
+
+    public static void setTotalCount(int totalCount) {
+        Car.totalCount = totalCount;
+    }
+
+    public int getInstanceCount() {
+        return instanceCount;
+    }
+
+    public void setInstanceCount(int instanceCount) {
+        this.instanceCount = instanceCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "instanceCount=" + instanceCount + " " +
+                "totalCount=" + totalCount +
+                '}';
+    }
 }
--- src/main/java/de/supernerd/Main.java
+++ src/main/java/de/supernerd/Main.java
@@ -1,4 +1,21 @@
 package de.supernerd;
 
 public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.incrementInstanceCount();
+        Car.incrementTotalCount();
+        System.out.println(car1);
+
+        Car car2 = new Car();
+        car2.incrementInstanceCount();
+        Car.incrementTotalCount();
+        System.out.println(car2);
+
+        Car car3 = new Car();
+        car3.incrementInstanceCount();
+        Car.incrementTotalCount();
+        System.out.println(car3);
+
+    }
 }
